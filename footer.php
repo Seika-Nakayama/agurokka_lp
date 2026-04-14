4d4a79788cd25b36ad687871e37553eda89628a9
nakayama-55 <nakayama@glide.co.jp>
2026-04-14 09:15:38 +0900

    test

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer class="site-footer">
+	<svg width="1806" height="88" viewBox="0 0 1806 88" fill="none" xmlns="http://www.w3.org/2000/svg" class="scroll-svg bod-svg"><path d="M0.0825195 63.3779C0.0825195 63.3779 218.083 81.9559 566.583 30.1389C915.083 -21.6781 1371.41 78.6949 1371.41 78.6949C1370.79 56.8785 1371.01 44.2537 1372.28 21.0087L1399.03 30.1389C1408.67 33.0439 1418.64 30.1389 1420.39 23.0837C1422.15 16.0286 1414.81 10.6335 1405.49 9.38844C1396.17 8.14341 1380.85 13.508 1372.28 19.3486C1361.82 12.0812 1355.19 -0.157716 1344.23 1.08826C1333.27 2.33423 1329.48 8.5399 1332.39 16.4436C1335.3 24.3473 1340.28 23.9138 1342.47 23.9138C1344.67 23.9138 1361.44 22.7257 1371.41 21.0087C1368.82 44.2975 1370.71 56.654 1374.04 78.6949C1374.04 78.6949 1532.58 96.6676 1614.08 78.6949C1695.58 60.7223 1764.58 56.3779 1805.58 68.3779" stroke="#309E66" stroke-width="1"/></svg>
+	<div class="footer-top">
+		<div class="footer-logo">
+			<img src="images/logo/agurokka_logo.webp" alt="AguRokkaのロゴ">
+		</div>
+		<div class="footer-menu">
+				<ul class="footer-nav-list">
+					<li class="footer-nav-item has-child">
+						<a href="#about-us" class="footer-menu-category">AguRokkaについて</a>
+						<ul class="sub-footer-nav">
+							<li><a href="#about-us">AguRokkaとは</a></li>
+							<li><a href="#why-agurokka">AguRokkaの由来</a></li>
+						</ul>
+					</li>
+					<li class="footer-nav-item has-child">
+						<a href="#members" class="footer-menu-category">メンバー紹介</a>
+						<ul class="sub-footer-nav">
+							<li><a href="#members">メンバーインタビュー</a></li>
+							<li><a href="#all-mem-list">AguRokkaメンバー紹介</a></li>
+							<li><a href="#AguRokka-qanda">Q&A</a></li>
+						</ul>
+					</li>
+					<li class="footer-nav-item has-child">
+						<a href="#activity" class="footer-menu-category">活動情報</a>
+						<ul class="sub-footer-nav">
+							<li><a href="#activity">活動履歴</a></li>
+							<li><a href="#">トップへ戻る</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<ul class="footer-sns-menu">
+				<li class="footer-sns-insta">
+					<a href="https://www.instagram.com/agurokka_kumamoto/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="Instagramのアイコン"></a>
+				</li>
+				<li class="footer-sns-facebook">
+					<a href="https://www.facebook.com/p/%E3%81%8F%E3%81%BE%E3%82%82%E3%81%A8%E8%BE%B2%E6%A5%AD%E5%A5%B3%E5%8F%B2%E3%82%B3%E3%83%9F%E3%83%A5%E3%83%8B%E3%83%86%E3%82%A3%E3%83%BC-Agurokka-61551086036877/" target="_blank"><img src="images/logo/Facebook_Logo_Primary.webp" alt="Facebookのアイコン"></a>
+				</li>
+			</ul>
+			<div class="entory-link-img">
+				<a href="https://docs.google.com/forms/d/1y0TlJKK-Ip5nqA4ef7zxUNKzVR2Dzr_bWXMSe7v3G1A/viewform?edit_requested=true" target="_blank"><img src="images/main-img/entory-link.webp" alt="入会フォーム誘導バナー"></a>
+			</div>
+		</div>
+	<p class="copy-right">&copy;くまもと農業女史コミュニティAguRokka</p>
+</footer>
