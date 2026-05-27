170e8fe0f0e9520d811ef20be2731612020c0988
nakayama-55 <nakayama@glide.co.jp>
2026-05-27 18:09:28 +0900

    feat: Aboutセクションのレイアウトを実装

--- js/script1.js
+++ js/script1.js
@@ -131,7 +131,7 @@ const svgObserver = new IntersectionObserver(entries => {
 });
 
 svgs.forEach(svg => svgObserver.observe(svg));
-const fadeTargets = document.querySelectorAll('.how-agurokka__flexarea, .category, .js-display');
+const fadeTargets = document.querySelectorAll('.l-about__panel, .category, .js-display');
 const fadeObserver = new IntersectionObserver((entries) => {
 	entries.forEach(entry => {
 		if (entry.isIntersecting) {
