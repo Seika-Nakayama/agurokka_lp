3f7efe76b12abdf1951b84a36f3cbd7dde7f51e5
nakayama-55 <nakayama@glide.co.jp>
2026-06-01 13:07:22 +0900

    feat: メンバーインタビューのHTMLとアセットを更新
    スライダー構成をSwiper対応に書き直し。画像をPNGからJPGに差し替え、
    ナビゲーション用SVGアローを追加。

--- sections/member-interview.php
+++ sections/member-interview.php
@@ -1,69 +1,74 @@
-<section class="l-member-interview">
+<section class="l-interview">
 	<h2 class="contents-title">
 		<img src="images/icon/flower.svg" alt="肥後六花モチーフアイコン" class="flower-icon">メンバーインタビュー
 	</h2>
-	<div class="l-member-interview__inner">
-		<div class="c-member-slider">
-			<section id="hondanouen" class="l-member-interview__item l-member-interview__item--01">
-				<div class="l-member-interview__image">
-					<figure class="p-member__face">
-						<img src="images/main-img/member1.webp" alt="本田農園、本田あゆ子さんの画像">
-					</figure>
-					<p class="p-member__subtitle p-member__subtitle--vertical">人との繋がりが</p>
-					<p class="p-member__subtitle p-member__subtitle--vertical">仕事のやりがいに</p>
-					<p class="p-member__sns"><a href="https://www.instagram.com/with_farm2022/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="Instagramのアイコン"></a></p>
-				</div>
-				<div class="l-member-interview__body">
-					<p class="p-member__farm">本田農園</p>
-					<h3 class="p-member__name">本田&nbsp;あゆ子<span class="p-member__name-end">さん</span></h3>
-					<p class="p-member__intro">実際入会してみたら想像以上に素敵な方ばかりで、勉強になります。色々な詳しいお話も惜しみなく教えてくださるし、楽しいこと、苦労されたことも参考になりますし、アドバイスもたくさんもらえます。でも実はただただ楽しいだけの会かもしれません。</p>
-				</div>
-			</section>
-			<section id="yatsushiromizuta" class="l-member-interview__item l-member-interview__item--02 l-member-interview__item--reverse">
-				<div class="l-member-interview__image">
-					<figure class="p-member__face">
-						<img src="images/main-img/member2.webp" alt="ヤツシロ水田Earm、水田さつきさんの画像">
-					</figure>
-					<p class="p-member__subtitle">1人では見えない</p>
-					<p class="p-member__subtitle">選択肢をもらえる場所</p>
-					<p class="p-member__sns"><a href="https://www.instagram.com/yatsushiro_mizutafarm/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="Instagramのアイコン"></a></p>
-				</div>
-				<div class="l-member-interview__body">
-					<p class="p-member__farm">ヤツシロミズタEarm</p>
-					<h3 class="p-member__name">水田&nbsp;さつき<span class="p-member__name-end">さん</span></h3>
-					<p class="p-member__intro">これまではひとりで悩んでいたことも、同じ志を持つ仲間に相談できるようになり、「自分だけじゃなかったんだ」と感じられる場ができました。仕事の視野が広がり、考え方や選択肢も増え、以前よりも前向きに、新しい挑戦への一歩を踏み出しやすくなりました。</p>
-				</div>
-			</section>
-			<section id="miyashitanouen" class="l-member-interview__item l-member-interview__item--03">
-				<div class="l-member-interview__image">
-					<figure class="p-member__face">
-						<img src="images/main-img/member3.webp" alt="みやした農園、宮下真由美さんの画像">
-					</figure>
-					<p class="p-member__subtitle p-member__subtitle--vertical">SNS、そして友達</p>
-					<p class="p-member__subtitle p-member__subtitle--vertical">この輪を私も繋げよう</p>
-					<p class="p-member__sns"><a href="https://www.instagram.com/miyashita.nouenn/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="Instagramのアイコン"></a></p>
-				</div>
-				<div class="l-member-interview__body">
-					<p class="p-member__farm">みやした農園</p>
-					<h3 class="p-member__name">宮下&nbsp;真由美<span class="p-member__name-end">さん</span></h3>
-					<p class="p-member__intro">AguRokkaを立ち上げられた水俣の永井さんのSNSで呼びかけを見て、以前から永井さんとは知り合いだったこともあり参加を即決しました。メンバーも個性豊かで面白い団体になりそうだと思いました。住む地域も栽培する作物も違うメンバーと交流することで、自分の農業知識や考え方、視野が広がるのを実感しています。</p>
-				</div>
-			</section>
-			<section id="latonouen" class="l-member-interview__item l-member-interview__item--04 l-member-interview__item--reverse">
-				<div class="l-member-interview__image">
-					<figure class="p-member__face">
-						<img src="images/main-img/member4.webp" alt="らと農園、森本和さんの画像">
-					</figure>
-					<p class="p-member__subtitle">仕事じゃない私も</p>
-					<p class="p-member__subtitle">大切にできる場所</p>
-					<p class="p-member__sns"><a href="https://www.instagram.com/yatsushiro_mizutafarm/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="Instagramのアイコン"></a></p>
-				</div>
-				<div class="l-member-interview__body">
-					<p class="p-member__farm">らと農園/株式会社LaTo</p>
-					<h3 class="p-member__name">森本&nbsp;和<span class="p-member__name-end">さん</span></h3>
-					<p class="p-member__intro">世代を超えた仲間が増え、農業をする女性について考えるきっかけが生まれました。みんなの話を聞き「私も頑張ろう」と背中を押され、農業女性のフェアやイベントに参加する楽しさも知りました。結婚前のように街へ飲みに行く機会もでき、仕事もプライベートも大切にできていると感じています。そのことを農業をする全ての人に伝え、手を伸ばしていきたいです。</p>
-				</div>
-			</section>
+	<div class="l-interview">
+		<div class="c-member-slider swiper">
+			<div class="swiper-wrapper">
+				<section id="hondanouen" class="swiper-slide l-interview__item l-interview__item--01">
+					<div class="l-interview__image">
+						<figure class="p-member__face">
+							<img src="images/main-img/interviews/interview01.jpg" alt="本田農園、本田あゆ子さんの画像">
+							<div class="p-member__sns"><a href="https://www.instagram.com/with_farm2022/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="公式Instagramを開く"></a></div>
+						</figure>
+						<p class="p-tag--01a p-tag--vertical">人との繋がりが</p>
+						<p class="p-tag--01b p-tag--vertical">仕事のやりがいに</p>
+					</div>
+					<div class="l-interview__body p-comment01">
+						<p class="p-member__farm">本田農園</p>
+						<h3 class="p-member__name">本田あゆ子<span class="p-member__name-end">さん</span></h3>
+						<p class="p-member__intro">実際入会してみたら想像以上に素敵な方ばかりで、勉強になります。色々な詳しいお話も惜しみなく教えてくださるし、楽しいこと、苦労されたことも参考になりますし、アドバイスもたくさんもらえます。でも実はただただ楽しいだけの会かもしれません。</p>
+					</div>
+				</section>
+				<section id="yatsushiromizuta" class="swiper-slide l-interview__item l-interview__item--02 l-interview__item--reverse">
+					<div class="l-interview__image">
+						<figure class="p-member__face">
+							<img src="images/main-img/interviews/interview02.jpg" alt="ヤツシロ水田Earm、水田さつきさんの画像">
+							<div class="p-member__sns"><a href="https://www.instagram.com/yatsushiro_mizutafarm/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="公式Instagramを開く"></a></div>
+						</figure>
+						<p class="p-tag--02a p-tag--vertical">1人では見えない</p>
+						<p class="p-tag--02b p-tag--vertical">選択肢をもらえる場所</p>
+					</div>
+					<div class="l-interview__body p-comment02">
+						<p class="p-member__farm">ヤツシロミズタEarm</p>
+						<h3 class="p-member__name">水田さつき<span class="p-member__name-end">さん</span></h3>
+						<p class="p-member__intro">これまではひとりで悩んでいたことも、同じ志を持つ仲間に相談できるようになり、「自分だけじゃなかったんだ」と感じられる場ができました。仕事の視野が広がり、考え方や選択肢も増え、以前よりも前向きに、新しい挑戦への一歩を踏み出しやすくなりました。</p>
+					</div>
+				</section>
+				<section id="miyashitanouen" class="swiper-slide l-interview__item l-interview__item--03">
+					<div class="l-interview__image">
+						<figure class="p-member__face">
+							<img src="images/main-img/interviews/interview03.jpg" alt="みやした農園、宮下真由美さんの画像">
+							<div class="p-member__sns"><a href="https://www.instagram.com/miyashita.nouenn/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="公式Instagramを開く"></a></div>
+						</figure>
+						<p class="p-tag--03a p-tag--vertical">SNS、そして友達</p>
+						<p class="p-tag--03b p-tag--vertical">この輪を私も繋げよう</p>
+					</div>
+					<div class="l-interview__body p-comment03">
+						<p class="p-member__farm">みやした農園</p>
+						<h3 class="p-member__name">宮下真由美<span class="p-member__name-end">さん</span></h3>
+						<p class="p-member__intro">AguRokkaを立ち上げられた水俣の永井さんのSNSで呼びかけを見て、以前から永井さんとは知り合いだったこともあり参加を即決しました。メンバーも個性豊かで面白い団体になりそうだと思いました。住む地域も栽培する作物も違うメンバーと交流することで、自分の農業知識や考え方、視野が広がるのを実感しています。</p>
+					</div>
+				</section>
+				<section id="latonouen" class="swiper-slide l-interview__item l-interview__item--04 l-interview__item--reverse">
+					<div class="l-interview__image">
+						<figure class="p-member__face">
+							<img src="images/main-img/interviews/interview04.jpg" alt="らと農園、森本和さんの画像">
+							<div class="p-member__sns"><a href="https://www.instagram.com/yatsushiro_mizutafarm/" target="_blank"><img src="images/logo/Instagram_Glyph_Gradient.webp" alt="公式Instagramを開く"></a></div>
+						</figure>
+						<p class="p-tag--04a">仕事じゃない私も</p>
+						<p class="p-tag--04b">大切にできる場所</p>
+					</div>
+					<div class="l-interview__body p-comment04">
+						<p class="p-member__farm">らと農園/株式会社LaTo</p>
+						<h3 class="p-member__name">森本和<span class="p-member__name-end">さん</span></h3>
+						<p class="p-member__intro">世代を超えた仲間が増え、農業をする女性について考えるきっかけが生まれました。みんなの話を聞き「私も頑張ろう」と背中を押され、農業女性のフェアやイベントに参加する楽しさも知りました。結婚前のように街へ飲みに行く機会もでき、仕事もプライベートも大切にできていると感じています。そのことを農業をする全ての人に伝え、手を伸ばしていきたいです。</p>
+					</div>
+				</section>
+			</div>
+			<button class="swiper-btn-prev" aria-label="前のスライド"></button>
+			<button class="swiper-btn-next" aria-label="次のスライド"></button>
+			<div class="swiper-pagination"></div>
 		</div>
 	</div>
 </section>
